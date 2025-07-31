6b163d69e16755ae4461b398cd5391a2f3097f77
hibaanadani <hibaanadani1@gmail.com>
2025-07-31 21:36:54 +0300

    created the product card with its css

---
+++ snackies-client/src/Components/ProductCard/ProductCard.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import './style.css';
+import starRating from './starRating';
+
+const ProductCard = (productID, productName, productPrice, productRating, productImage) => {
+    const navigate = useNavigate;
+
+    const handleCardClick =()=>{
+        navigate(`/product/${productID}`);
+    };
+
+    return (
+        <div className="product-card" onClick={handleCardClick}>
+            <div className="product-image">
+                <img src={productImage} alt={productImage} />
+            </div>    
+            <div className="product-details">
+                <starRating rating={productRating} />
+                <h3 className="product-name">{productName}</h3>
+                <p className="product-price">${productPrice}</p>
+            </div>
+        </div>
+    );
+}
+export default ProductCard;
