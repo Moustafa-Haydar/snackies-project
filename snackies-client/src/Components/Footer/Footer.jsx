80fa589035a5f244d8d7dae1ff20137c75df709f
hibaanadani <hibaanadani1@gmail.com>
2025-07-31 12:34:01 +0300

    created Footer component with styling

---
+++ snackies-client/src/Components/Footer/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+import Link from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'
+
+const Footer = () =>{
+    const navigate= useNavigate();
+        return(
+            <footer className='footer-container'>
+                <div className='main-footer'>
+                <div className='about-us-section'>
+                    <h3><Link to="/about-us">About</Link></h3>
+                    <img src="logo" alt="snackies-logo" className='footer-logo'/>
+                    <p>Learn more about our mission, and some frequently asked questions!</p>
+                </div>
+                <div className=' shop-section'>
+                    <h3><Link to='/shop'>Shop</Link></h3>
+                    <p onClick={() => navigate('/shop/cheesy')}>Cheesy</p>
+                    <Link to="/shop/cheesy">Cheesy</Link>
+                    <Link to="/shop/chocolaty">Chocolaty</Link>
+                    <Link to="/shop/nutty">Nutty</Link>
+                </div>
+                <div className='socials-section'>
+                    <h3>Socials</h3>
+                    <p>Instagram</p>
+                    <p>Facebook</p>
+                    <p>Youtube</p>
+                </div>
+                <div className='profile-section'>
+                    <h3><Link to='/profile'>Profile</Link></h3>
+                    <Link to='/profile'>Your Profile</Link>
+                    <Link to='/cart'>Cart</Link>
+                    <Link to='/checkout'>Checkout</Link>
+                </div>
+                </div>
+                <div className='logo-section'>
+                    <h1 className='logo-section-footer'>Snackies</h1>
+                </div>
+            </footer>
+        );
+}
+export default Footer;
