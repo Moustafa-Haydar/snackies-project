69b66ae2612295e68b45b226b2a877c5174e04ea
hibaanadani <hibaanadani1@gmail.com>
2025-08-01 23:37:46 +0300

    added try and catch for the product controller

--- snackies-client/src/Controllers/ProductsController.jsx
+++ snackies-client/src/Controllers/ProductsController.jsx
@@ -4,15 +4,19 @@ class ProductsController {
   static async getAllProducts(setProducts) {
     try {
       const response = await axios.get('http://127.0.0.1:8000/api/v0.1/shop/products');
-      
       const products = response.data.payload;
+
+      if (response.status === 200) {
       setProducts(products);
 
       // debug debug
       console.log(products);
 
-    } catch (error) {
-      console.log(error);
+    }else{
+       console.error(`Error fetching products: ${response.status}`);
+    }}
+     catch (error) {
+      console.log( console.error(`Error fetching categories: ${error.message}`));
     }
   }
 }
