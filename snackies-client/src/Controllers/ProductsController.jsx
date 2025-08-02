82fce73e453a28f5817c46e04971cfa145912666
hibaanadani <hibaanadani1@gmail.com>
2025-08-02 04:04:12 +0300

    added Daniels uploadImage method to my getAllProducts

--- snackies-client/src/Controllers/ProductsController.jsx
+++ snackies-client/src/Controllers/ProductsController.jsx
@@ -19,5 +19,18 @@ class ProductsController {
       console.log( console.error(`Error fetching categories: ${error.message}`));
     }
   }
+    static async uploadImage(base64) {
+    try {
+      const response = await axios
+        .post("http://127.0.0.1:8000/api/v0.1/test/upload_image", {
+          base64: base64,
+          item_id: 0,
+        })
+  
+        console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 export default ProductsController;
