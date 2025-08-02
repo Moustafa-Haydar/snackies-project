f9e65c31623d2c8349967194d82f84cb189ce205
Danny <dhayek92@gmail.com>
2025-08-02 12:56:57 +0300

    Created basic styling and layout for product page

--- snackies-client/src/Pages/ProductPage/ProductPage.jsx
+++ snackies-client/src/Pages/ProductPage/ProductPage.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
 import './style.css'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import Button from '../../Components/Button/Button'
 
 const ProductPage = () => {
   return (
     <div>
-        <div>
-            product image
+        <Header />
+
+        <div className='flex product-page-content'>
+            <div className='flex product-page-image'>
+                product image
+            </div>
+
+            <div className='flex column product-page-info'>
+                <h1>Asian Snack Mix</h1>
+
+                <p>Rating</p>
+
+                <h3>Price</h3>
+
+                <Button btn_name={"Add to Cart"} />
+
+                <p>Add to Favotires</p>
+
+                <p>This is a gift</p>
+
+                <h4>Product Details</h4>
+
+                <p>Product Description</p>
+            </div>
         </div>
 
-        <div>
-            Product Info
-        </div>
+        <Footer />
     </div>
   )
 }
