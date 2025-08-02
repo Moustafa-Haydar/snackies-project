06da75f1565aabdd3c01853050a9123c06185a13
hibaanadani <hibaanadani1@gmail.com>
2025-08-02 14:33:02 +0300

    header is now as wanted

--- snackies-client/src/Components/Header/Header.jsx
+++ snackies-client/src/Components/Header/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import Navbar from '../Navbar/navbar.jsx';
 import './style.css';
-import userIcon from '../../Assets/icons/circle-user-solid-full.svg';
-import cartIcon from '../../Assets/icons/cart-shopping-solid-full.svg';
+import userIcon from '../../Assets/Icons/circle-user-solid-full.svg';
+import cartIcon from '../../Assets/Icons/cart-shopping-solid-full.svg';
+import snackiesLogo from '../../Assets/logos/snackies-logo-orange-nobg.webp';
 
 
 const Header = () => {
@@ -17,7 +18,7 @@ const Header = () => {
             <div className='header-content'>
                 <div className='logo-section'>
                     <Link className='logo-link' to="/">
-                        <img src="../../Assets/logos/snackies-logo-orange-nobg.webp" alt="logo" className='logo-img'/>
+                        <img src={snackiesLogo} alt="logo" className='logo-img'/>
                     </Link>
                 </div>
                 <div className='nav-section'>
@@ -25,7 +26,7 @@ const Header = () => {
                 </div>
                 <div className='icons-section'>
                     <img src={userIcon} alt="userIcon" onClick={() => navigate('/login')}/>
-                     <img src={cartIcon} alt="cartIcon" onClick={() => navigate('/cart')} />
+                    <img src={cartIcon} alt="cartIcon" onClick={() => navigate('/cart')} />
                  </div>
             </div>
         </header>
