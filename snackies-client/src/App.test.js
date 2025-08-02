71e39ab6e594a3bd75c6c06ce04f549693fe140a
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-08-02 18:34:27 +0300

    update setupTests to test ci

--- snackies-client/src/App.test.js
+++ snackies-client/src/App.test.js
@@ -6,9 +6,6 @@ jest.mock('axios', () => ({
   post: jest.fn(),
 }));
 
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
