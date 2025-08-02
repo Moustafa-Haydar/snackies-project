d2f8610d3a2ccdab28d61cd3e05898362712c2b2
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-08-02 18:31:49 +0300

    update setupTests to test ci

--- snackies-client/src/App.test.js
+++ snackies-client/src/App.test.js
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+jest.mock('axios', () => ({
+  get: jest.fn(),
+  post: jest.fn(),
+}));
+
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-jest.mock('axios', () => ({
-  get: jest.fn(),
-  post: jest.fn(),
-}));
+test('renders header quote', () => {
+  render(<App />);
+  const quoteElement = screen.getByText(/a snack a day keeps the hunger away/i);
+  expect(quoteElement).toBeInTheDocument();
+});
