811d5b3ae29436ac4156baaf489ea30448a5772e
hibaanadani <hibaanadani1@gmail.com>
2025-08-03 12:26:30 +0300

    did ajustments to the category api src, star review, and its css

--- snackies-client/src/Components/CategoryCard/CategoryCard.jsx
+++ snackies-client/src/Components/CategoryCard/CategoryCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../Button/Button";
 import './style.css';
 
-const CategoryCard =({imageSrc, categoryName, onClick, type="secondary"})=>{
+const CategoryCard =({imageSrc, categoryName, onClick, type="outline"})=>{
     return (
         <div className="categorycard-component">
             <div className="image-section">
