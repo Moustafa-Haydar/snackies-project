756f9933de454e20ef7cada2f06e73a6a85db768
hibaanadani <hibaanadani1@gmail.com>
2025-08-03 15:43:44 +0300

    done with the shop page with filter

--- snackies-client/src/Components/ShopFilter/ShopFilter.jsx
+++ snackies-client/src/Components/ShopFilter/ShopFilter.jsx
@@ -1,45 +1,29 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import './style.css';
-import filter from '../../Assets/Icons/filter-solid-full.svg'
 
-const ShopFilter = ({name, choices = [], onSelect}) => {
-
-    const filterRef = useRef(null);
-
-    const toggleFromRight = () => {
-        if (filterRef.current) {
-            filterRef.current.classList.toggle("show");
-        }
-    };
-
-    return ( 
-
-        <div className="rightFilter"
-            onMouseEnter={toggleFromRight}
-            onMouseLeave={toggleFromRight}>
-
-            <div className="row center align-center gap-2"
-            >
-                <button className='filterbtn'>
-                    {name}
-                    <img className='filter-icon ' src={filter} alt="filter-icon" />
-                </button>
-            </div>
-
-            <div ref={filterRef} className="rightFilter-content">
+const ShopFilter = ({ name, type = 'checkbox', choices = [], selectedChoice, onSelect }) => {
+    return (
+        <div className="rightFilter-group">
+            <span className="filter-label">{name}</span>
+            <div className="rightFilter-content">
                 {choices.map((choice) => (
-                    <a href="..." 
-                    key={choice}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        onSelect(choice);
-                    }}
-                    >{choice}</a>
-                    ))}
+                    <div key={choice} className="checkbox-option">
+                        <input
+                            type={type}
+                            id={`${name}-${choice}`}
+                            name={name}
+                            value={choice}
+                            checked={selectedChoice === choice}
+                            onChange={() => onSelect(choice)}
+                        />
+                        <label htmlFor={`${name}-${choice}`}>
+                            {choice}
+                        </label>
+                    </div>
+                ))}
             </div>
-
         </div>
-     );
-}
- 
+    );
+};
+
 export default ShopFilter;
