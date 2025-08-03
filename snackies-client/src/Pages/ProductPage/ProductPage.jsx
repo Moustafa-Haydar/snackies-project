c3573d5a87447e6cd4f2e976135df6100fe98eec
Danny <dhayek92@gmail.com>
2025-08-03 15:39:27 +0300

    Made changes to landing page with new components and added more functionality to product page

--- snackies-client/src/Pages/ProductPage/ProductPage.jsx
+++ snackies-client/src/Pages/ProductPage/ProductPage.jsx
@@ -4,7 +4,10 @@ import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import Button from "../../Components/Button/Button";
 
-
+import heartFull from "../../Assets/Icons/heart-solid.svg";
+import heartEmpty from "../../Assets/Icons/heart-outline.svg";
+import giftFull from "../../Assets/Icons/square-solid.svg";
+import giftEmpty from "../../Assets/Icons/square-outline.svg";
 
 import { TokenContext } from "../../Contexts/TokenContext";
 
@@ -23,6 +26,9 @@ const ProductPage = ({
   const { tokenState } = useContext(TokenContext);
   const [ userState, setUserState ] = useState(null);
 
+  const [ favIcon, setFavIcon ] = useState(heartEmpty);
+  const [ giftIcon, setGiftIcon ] = useState(giftEmpty);
+
   useEffect( () => {
         TokenController.decodeToken(tokenState, setUserState);
     }, [tokenState]);
@@ -31,6 +37,30 @@ const ProductPage = ({
     console.log("adding this item to cart");
     CartController.addItemToCart(productId, userState.id);
   };
+
+  const test = () => {
+    console.log("test");
+  }
+
+  const swapFav = () => {
+    if (favIcon == heartEmpty) {
+      console.log("Adding to Favorites");
+      setFavIcon(heartFull);
+    } else {
+      console.log("Removing to Favorites");
+      setFavIcon(heartEmpty);
+    }
+  }
+
+    const swapGift = () => {
+    if (giftIcon == giftEmpty) {
+      console.log("This is a gift");
+      setGiftIcon(giftFull);
+    } else {
+      console.log("This is not a gift");
+      setGiftIcon(giftEmpty);
+    }
+  }
 
   return (
     <div>
@@ -56,12 +86,16 @@ const ProductPage = ({
 
           <Button btn_name={"Add to Cart"} onClick={addItemCart} />
 
-          <div>
-            <div>
+          <div className="flex column product-page-buttons-div">
+            <button onClick={swapFav} className="flex product-page-button">
+              <img src={favIcon} alt="icon" className="product-button-icon"></img>
               <p>Add to Favorites</p>
-            </div>
+            </button>
 
-            <p>This is a gift</p>
+            <button onClick={swapGift} className="flex product-page-button">
+              <img src={giftIcon} alt="icon" className="product-button-icon"></img>
+              <p>This is a gift</p>
+            </button>
           </div>
 
           <div>
